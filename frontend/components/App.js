96164e953fc1a68e201c67e042bb667faff2f430
Pamir Perdeci <pamirperdeci@gmail.com>
2023-03-10 12:05:07 -0500

    able to change state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,23 +1,48 @@
 import React from 'react'
 
+
+const initialItems = [  {
+  name: 'Organize Garage',
+  id: 1528817077286, // could look different, you could use a timestamp to generate it
+  completed: false
+},
+{
+  name: 'Bake Cookies',
+  id: 1528817084358,
+  completed: false
+}
+]
+
+
 export default class App extends React.Component {
-  
+  constructor(){
+    super();
+    this.state = {
+      items: initialItems
+  }
+      }
+
+    addItem =(e, item)=>{
+      e.preventDefault();
+      const newItem = {
+        name: item,
+        id: Date.now(),
+        completed: false
+      }
+    this.setState({...this.state.items, items: [...this.state.items, newItem]})
+    }
 
   render() {
     return (
       <div>
         <h1>Todos:</h1>
-        <p>Walk the dog</p>
-        <p>Learn React</p>
-        <p>Have Fun</p>
+        
         <div>
         <label>
         <input type="text" ></input>
 
         </label>
-        <button>Submit</button>
-        
-
+        <button onClick={this.addItem}>Submit</button>
         </div>
         <button>Hide Completed</button>
       </div>
