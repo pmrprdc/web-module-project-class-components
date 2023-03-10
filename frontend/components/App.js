14be44130fe62f52609f0ce258c618cca17c4ea6
Pamir Perdeci <pamirperdeci@gmail.com>
2023-03-10 10:39:30 -0500

    built the app with static elements

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,7 +6,20 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todos:</h1>
+        <p>Walk the dog</p>
+        <p>Learn React</p>
+        <p>Have Fun</p>
+        <div>
+        <label>
+        <input type="text" ></input>
+
+        </label>
+        <button>Submit</button>
+        
+
+        </div>
+        <button>Hide Completed</button>
       </div>
     )
   }
